aaf5a1a94f15f73d3cb003de693e473a4bde1c2f
aguilanbon <aguilanbon@gmail.com>
2022-06-08 11:13:06 +0800

    added sign in w/ google button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,24 @@
 function Login({ setFormState, }) {
     return (
-        <form action="">
-            <div className="form-group">
-                <label htmlFor="email">Email @</label>
-                <input type="email" name="email" id="log1" className='inputBox' required autoComplete='off' />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password &#8226;&#8226;&#8226; </label>
-                <input type="password" name="" id="log2" className='inputBox' required autoComplete='off' />
-            </div>
-            <div className="form-group">
-                <input type="submit" value="Log In" />
-            </div>
-            <div className="form-footer">
-                <p>Not yet signed up? <span style={linkStyle} onClick={() => setFormState('signup')}>Click Here!</span></p>
-            </div>
-        </form>
+        <div className="googleAuth-container">
+            <button>Sign in with Google</button>
+            <form action="">
+                <div className="form-group">
+                    <label htmlFor="email">Email @</label>
+                    <input type="email" name="email" id="log1" className='inputBox' required autoComplete='off' />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password &#8226;&#8226;&#8226; </label>
+                    <input type="password" name="" id="log2" className='inputBox' required autoComplete='off' />
+                </div>
+                <div className="form-group">
+                    <input type="submit" value="Log In" />
+                </div>
+                <div className="form-footer">
+                    <p>Not yet signed up? <span style={linkStyle} onClick={() => setFormState('signup')}>Click Here!</span></p>
+                </div>
+            </form>
+        </div>
     )
 }
 
