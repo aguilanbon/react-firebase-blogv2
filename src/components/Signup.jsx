979caf5db682b4a83792796e174ce58133fc2be5
aguilanbon <aguilanbon@gmail.com>
2022-06-09 11:40:54 +0800

    added framer-motion

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { auth } from '../firebase-config'
+import { useNavigate } from 'react-router-dom'
 
 function Signup({ setUserMessage, setFormState }) {
 
