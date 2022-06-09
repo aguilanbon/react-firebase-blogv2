21fc605d9f55d7017679d9dd461ccdae92e7d95e
aguilanbon <aguilanbon@gmail.com>
2022-06-09 15:41:39 +0800

    alert cards on login

--- src/pages/UserForm.jsx
+++ src/pages/UserForm.jsx
@@ -5,17 +5,18 @@ import Signup from '../components/Signup'
 function UserForm({ formState, setFormState, setIsAuth }) {
 
     const [userMessage, setUserMessage] = useState('')
+    const [messageColor, setMessageColor] = useState('')
 
     return (
         <div className='user-form'>
             {userMessage === '' ? '' :
-                <div className="userMessage-container">
+                <div className={`userMessage-container ${messageColor}`}>
                     <h2>{userMessage}</h2>
                 </div>
             }
             <div className="form-container">
-                {formState === 'login' && <Login setFormState={setFormState} setIsAuth={setIsAuth} setUserMessage={setUserMessage} />}
-                {formState === 'signup' && <Signup setFormState={setFormState} setUserMessage={setUserMessage} />}
+                {formState === 'login' && <Login setFormState={setFormState} setIsAuth={setIsAuth} setUserMessage={setUserMessage} setMessageColor={setMessageColor} />}
+                {formState === 'signup' && <Signup setFormState={setFormState} setUserMessage={setUserMessage} setMessageColor={setMessageColor} />}
             </div>
         </div>
     )
