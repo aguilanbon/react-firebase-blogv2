485d243302c286c6bf40772cfe9738671103c7a0
aguilanbon <aguilanbon@gmail.com>
2022-06-09 17:11:42 +0800

    added image upload

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
-function BlogCard({ title, content, author }) {
+function BlogCard({ title, content, author, uri }) {
     return (
         <motion.div whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }} className='blog-card'>
             <div className="card-container">
-                {/* <div className="card-container__image">
-                    <img src="../z.png" alt="" />
-                </div> */}
+                <div className="card-container__image">
+                    <img src={uri} alt="" />
+                </div>
                 <motion.div animate={{ x: [-150, 0] }} transition={{ duration: 1 }} className="card-container__content">
                     <div className="card-container__content__title">
                         <h1>{title}</h1>
