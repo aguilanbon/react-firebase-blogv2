866f24974531e7c310f4745c7dc95d8cf24b73ff
aguilanbon <aguilanbon@gmail.com>
2022-06-15 19:28:48 +0800

    clickable blogcards

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
+
 // import { doc, deleteDoc } from 'firebase/firestore'
 // import { db, auth } from '../firebase-config'
 
-function BlogCard({ title, content, author, uri }) {
+function BlogCard({ title, content, author, uri, postId }) {
 
     // const deletePost = async (id) => {
     //     await deleteDoc(doc(db, 'posts', id))
@@ -12,26 +14,33 @@ function BlogCard({ title, content, author, uri }) {
 
     return (
         <motion.div whileHover={{ scale: 1.1 }} className='blog-card'>
-            <div className="card-container">
-                <motion.div animate={{ opacity: [0, 1] }} transition={{ duration: 2 }} className="card-container__image">
-                    <img loading='lazy' src={uri} alt="" />
-                </motion.div>
-                <motion.div animate={{ x: [-150, 0] }} transition={{ duration: 1 }} className="card-container__content">
-                    <div className="card-container__content__title">
-                        <h1>{title}</h1>
-                    </div>
-                    <div className="card-container__content__body">
-                        <p style={{ whiteSpace: 'pre-wrap' }}>
-                            {content}
-                        </p>
-                    </div>
-                    <div className="card-container__content__footer">
-                        <p>By: {author.name}</p>
-                    </div>
-                </motion.div>
-            </div>
-        </motion.div>
+            <Link style={cardStyle} to={`/blog/${postId}`}>
+                <div className="card-container">
+                    <motion.div animate={{ opacity: [0, 1] }} transition={{ duration: 2 }} className="card-container__image">
+                        <img loading='lazy' src={uri} alt="" />
+                    </motion.div>
+                    <motion.div animate={{ x: [-150, 0] }} transition={{ duration: 1 }} className="card-container__content">
+                        <div className="card-container__content__title">
+                            <h1>{title}</h1>
+                        </div>
+                        <div className="card-container__content__body">
+                            <p style={{ whiteSpace: 'pre-wrap' }}>
+                                {content}
+                            </p>
+                        </div>
+                        <div className="card-container__content__footer">
+                            <p>By: {author.name}</p>
+                        </div>
+                    </motion.div>
+                </div>
+            </Link>
+            </motion.div>
     )
+}
+
+const cardStyle = {
+    textDecoration: 'none',
+    color: 'black'
 }
 
 export default BlogCard
