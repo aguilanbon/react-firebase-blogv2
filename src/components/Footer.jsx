38306143503802d376df8486a3eba17937aa8fc4
aguilanbon <aguilanbon@gmail.com>
2022-06-17 19:37:55 +0800

    added footer & alert fixes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <footer>@ Bon Aguilan | Front End Developer</footer>
+    </div>
+  )
+}
+
+export default Footer
