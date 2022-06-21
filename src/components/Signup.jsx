a905fd29406f6004345a00b2c374c37fd7a7c85e
aguilanbon <aguilanbon@gmail.com>
2022-06-21 12:11:47 +0800

    minor ui fixes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -32,18 +32,18 @@ function Signup({ setUserMessage, setFormState, setMessageColor }) {
         }}>
             <div className="form-group">
                 <label htmlFor="name">Full Name <span style={{ fontSize: '.7em', opacity: '.7' }}>'Ethan Hunt'</span></label>
-                <input type="text" name="name" id="su" className='inputBox' required autoComplete='off' onChange={e => setSignUpName(e.target.value)} />
+                <input type="text" name="name" id="su" className='inputBox' required autoComplete='off' placeholder='Enter full name...' onChange={e => setSignUpName(e.target.value)} />
             </div>
             <div className="form-group">
                 <label htmlFor="email">Email @</label>
-                <input type="email" name="email" id="su1" className='inputBox' required autoComplete='off' onChange={e => setSignUpEmail(e.target.value)} />
+                <input type="email" name="email" id="su1" className='inputBox' required autoComplete='off' placeholder='Enter a valid email...' onChange={e => setSignUpEmail(e.target.value)} />
             </div>
             <div className="form-group">
                 <label htmlFor="password">Password &#8226;&#8226;&#8226; </label>
-                <input type="password" name="" id="" className='inputBox' autoComplete='off' required onChange={e => setSignUpPassword(e.target.value)} />
+                <input type="password" name="" id="" className='inputBox' autoComplete='off' placeholder='******' required onChange={e => setSignUpPassword(e.target.value)} />
             </div>
             <div className="form-group">
-                <motion.input whileHover={{ scale: 1.1 }} whileTap={{ scaleX: 0.9 }}  type="submit" value="Sign up" />
+                <motion.input whileHover={{ scale: 1.1 }} whileTap={{ scaleX: 0.9 }} type="submit" value="Sign up" />
             </div>
             <div className="form-footer">
                 <p>You're almost ready to post a blog!</p>
