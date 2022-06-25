e421c4d505cb3b48e9446e5f221da6b4c3329398
aguilanbon <aguilanbon@gmail.com>
2022-06-25 11:27:47 +0800

    add simple validation if title and content is empty

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -17,6 +17,7 @@ function Create() {
     const [content, setContent] = useState('')
     const [isLoading, setIsLoading] = useState(false)
     const [image, setImage] = useState(null)
+    const [createError, setCreateError] = useState('')
 
     let imgURL = ''
 
@@ -28,20 +29,27 @@ function Create() {
         setIsLoading(true)
         setIsActive('')
         try {
-            await imageUpload()
-            await addDoc(postsCollection, {
-                title, content, imageURL: imgURL, createdAt: serverTimestamp(),
-                author: {
-                    name: auth.currentUser.displayName,
-                    id: auth.currentUser.uid
-                }
-            })
-            // toast.promise(response, {
-            //     loading: 'Loading',
-            //     success: 'Got the data',
-            //     error: 'Error when fetching',
-            // });
-            navigate('/')
+            if (title === '' || content === '') {
+                setCreateError('Blog must have both a title and content')
+                setIsLoading('post')
+            }
+            else if (title && content) {
+                await imageUpload()
+                await addDoc(postsCollection, {
+                    title, content, imageURL: imgURL, createdAt: serverTimestamp(),
+                    author: {
+                        name: auth.currentUser.displayName,
+                        id: auth.currentUser.uid
+                    }
+                })
+                // toast.promise(response, {
+                //     loading: 'Loading',
+                //     success: 'Got the data',
+                //     error: 'Error when fetching',
+                // });
+                navigate('/')
+            }
+
         } catch (error) {
             console.log(error);
         }
@@ -55,7 +63,6 @@ function Create() {
         imgURL = imgURI
     }
 
-
     useEffect(() => {
         if (!isAuth) {
             navigate('/')
@@ -64,12 +71,12 @@ function Create() {
 
     return (
         <motion.div animate={{ x: [-150, 0] }} transition={{ duration: 1 }} className='create-container'>
+            {createError === '' ? '' : <p className='create-error'>{createError}</p>}
             <div className="create-form-container">
                 <form action="" onSubmit={e => {
                     e.preventDefault()
                     createPost()
                 }}>
-
                     <label htmlFor="title">Blog Title</label>
                     <input type="text" name="title" id="" onChange={e => setTitle(e.target.value)} />
                     <label htmlFor="content">Content</label>
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -8,7 +8,7 @@ import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage
 import { v4 } from 'uuid';
 // import toast from 'react-hot-toast';
 
-function Edit({postId}) {
+function Edit({ postId }) {
 
   let id = useParams(postId)
   let navigate = useNavigate()
@@ -21,65 +21,65 @@ function Edit({postId}) {
   const [buttonState, setButtonState] = useState(false)
 
   const updatePost = async () => {
-      setButtonState(true)
-      try {
-        await imageUpload()
-        const updateRef = doc(db, 'posts', id.postId)
-        await updateDoc(updateRef, {
-              title, content, imageURL : imgURL === '' ? imgLink : imgURL
-        })
+    setButtonState(true)
+    try {
+      await imageUpload()
+      const updateRef = doc(db, 'posts', id.postId)
+      await updateDoc(updateRef, {
+        title, content, imageURL: imgURL === '' ? imgLink : imgURL
+      })
 
-        // toast.promise(updateDoc, {
-        //   success: 'Yay!',
-        //   error: 'Ughhh',
-        //   loading: 'Saving'
-        // })
-        navigate('/')
+      // toast.promise(updateDoc, {
+      //   success: 'Yay!',
+      //   error: 'Ughhh',
+      //   loading: 'Saving'
+      // })
+      navigate('/')
 
-     } catch (error) {
-        console.log(error);
-     }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const imageUpload = async () => {
-      if (image == null) return
+    if (image == null) return
 
-      const currentObjURL = ref(storage, imgLink)
-      const currentObjRef = ref(storage, currentObjURL.fullPath)
-      await deleteObject(currentObjRef)
+    const currentObjURL = ref(storage, imgLink)
+    const currentObjRef = ref(storage, currentObjURL.fullPath)
+    await deleteObject(currentObjRef)
 
-      const imageRef = ref(storage, `blogBanners/${image.name + v4()}`)
-      await uploadBytes(imageRef, image)
-      const imgURI = await getDownloadURL(imageRef)
-      imgURL = imgURI
+    const imageRef = ref(storage, `blogBanners/${image.name + v4()}`)
+    await uploadBytes(imageRef, image)
+    const imgURI = await getDownloadURL(imageRef)
+    imgURL = imgURI
   }
-  
+
   useEffect(() => {
     const getById = async () => {
-        const postRef = doc(db, 'posts', id.postId)
-        const response = await getDoc(postRef)
-        setTitle(response.data().title)
-        setContent(response.data().content)
-        setImgLink(response.data().imageURL)
+      const postRef = doc(db, 'posts', id.postId)
+      const response = await getDoc(postRef)
+      setTitle(response.data().title)
+      setContent(response.data().content)
+      setImgLink(response.data().imageURL)
     }
     getById()
-  },[id.postId])
+  }, [id.postId])
 
   return (
     <div className='edit-page__container'>
-        <div className="create-form-container">
-            <form onSubmit={(e) => {
-              e.preventDefault()
-              updatePost()
-            }}>
-                <label htmlFor="title">Blog Title</label>
-                <input type="text" name="title" id="" defaultValue={title} onChange={(e) => setTitle(e.target.value)} />
-                <label htmlFor="content">Content</label>
-                <textarea style={{ whiteSpace: 'pre-wrap' }} name="content" id="" cols="30" rows="15" defaultValue={content} onChange={(e) => setContent(e.target.value)}></textarea>
-                <input type="file" name="image" id="" onChange={e => setImage(e.target.files[0])}/>
-                <input type="submit" value={buttonState ? 'Updating...' : 'Update' } disabled={buttonState} />
-            </form>
-        </div>
+      <div className="create-form-container">
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          updatePost()
+        }}>
+          <label htmlFor="title">Blog Title</label>
+          <input type="text" name="title" id="" defaultValue={title} onChange={(e) => setTitle(e.target.value)} />
+          <label htmlFor="content">Content</label>
+          <textarea style={{ whiteSpace: 'pre-wrap' }} name="content" id="" cols="30" rows="15" defaultValue={content} onChange={(e) => setContent(e.target.value)}></textarea>
+          <input type="file" name="image" id="" onChange={e => setImage(e.target.files[0])} />
+          <input type="submit" value={buttonState ? 'Updating...' : 'Update'} disabled={buttonState} />
+        </form>
+      </div>
     </div>
   )
 }
